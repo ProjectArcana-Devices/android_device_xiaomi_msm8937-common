36037adf7113aec116d7a20c34aa24e35f0c651b
EXOdev-698 <maxim55miui11@gmail.com>
2021-12-15 20:17:30 +0300

    msm8937-common: Enable USB debugging and unsecure ADB by default
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -454,6 +454,13 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# ADB 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
