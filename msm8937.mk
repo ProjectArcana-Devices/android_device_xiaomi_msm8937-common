8f7a76b0ba1e7a64adcc2bcd3a1e887b87ab2691
ItsVixano <giovanniricca@protonmail.com>
2022-01-19 15:05:39 +0100

    msm8937-common: Provide gnss, power, displayservice on vendor
    Change-Id: I8bc8dcc9a4f962123eee3130f9de3ae467f40a98

--- msm8937.mk
+++ msm8937.mk
@@ -143,6 +143,7 @@ endif
 
 # Display
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
@@ -188,6 +189,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0.vendor
 endif
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.2.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss@3.0.vendor
 
 # Health
 PRODUCT_PACKAGES += \
@@ -283,6 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
