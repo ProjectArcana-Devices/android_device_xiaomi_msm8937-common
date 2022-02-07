33da7368b8714eeecb13153617a041fdb672c892
laleeroy <gabwielcastwo@gmail.com>
2022-02-07 09:39:06 +0800

    msm8937-common: update custom clang path
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,7 @@ TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
 TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-android-
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
