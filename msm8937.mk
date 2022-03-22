05c7cf7844bc3cd607b06ab52d1bf2e6e6cb4bb0
erfanoabdi <erfangplus@gmail.com>
2022-03-22 20:21:45 +0800

    msm8937-common: Prepare to move to in-tree HALs
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -359,6 +359,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# QCOM HALs
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
